02e4a5c33b61bbb1a892f422f1e000e287ce16df
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-24 01:12:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,7 +20,8 @@ def turn(board)
   #input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index)
+    current_player = current_player(board)
+    move(board, index, current_player)
     display_board(board)
   else
     turn(board)
@@ -43,7 +44,6 @@ end
 def move(board, index, current_player)
   #board[index] = current_player
   board[index] = current_player(board)
-  board[index] = current_player
 end
 
 def position_taken?(board, location)
