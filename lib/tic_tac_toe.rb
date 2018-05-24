3f47ab1c4f8cb88b54aee8bb3a52b26e74b4a9eb
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-24 01:14:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,12 +1,17 @@
 def play(board)
   until over?(board)
     turn(board)
+    if won?(board)
+      puts "Congratulations #{winner(board)}!"
+    elsif draw?(board)
+      puts "Cat's Game!"
+    end
   end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  elsif draw?(board)
-    puts "Cat's Game!"
-  end
+  #if won?(board)
+  #  puts "Congratulations #{winner(board)}!"
+  #elsif draw?(board)
+  #  puts "Cat's Game!"
+  #end
 end
 
 def turn(board)
