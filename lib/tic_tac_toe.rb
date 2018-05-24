fb56f1090f0c86ed18ea3fd349eefbdb118b9f9b
popechrisj@gmail.com <popechrisj@gmail.com>
2018-05-24 01:02:58 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,7 +35,7 @@ def input_to_index(user_input)
 end
 
 def move(board, index, current_player)
-#def move(board, index, current_player)
+#def move(board, index, current_player = "X")
   #board[index] = current_player
   board[index] = current_player(board)
 end
